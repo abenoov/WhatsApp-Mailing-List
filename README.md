e6726fc2d669188fdd12632c685a7372e83728d7
Mukhtar <m.abenov@pitech.kz>
2023-04-27 01:21:11 +0200

    feat: added readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mailing-list
+
 Simple WhatsApp mailing list script
+
+1. `npm install` <-- install dependencies
+2. fill `.env` variables [check developers.facebook.com](https://developers.facebook.com)
+3. `node index.js` <-- runs script
--- index.js
+++ index.js
@@ -4,59 +4,61 @@ const axios = require("axios");
 require("dotenv").config();
 
 const directory = "./data";
+
 const base_url = process.env.BASE_URL;
 const access_token = process.env.ACCESS_TOKEN;
 const phone_number_id = process.env.PHONE_NUMBER_ID;
 const version = process.env.VERSION;
-const file_link_path = "https://www.africau.edu/images/default/sample.pdf";
+
 const url = `${base_url}/${version}/${phone_number_id}/messages`;
 const customHeaders = {
 	"Content-Type": "application/json",
 	Authorization: "Bearer " + access_token,
 };
 
-const requestData = {
-	messaging_product: "whatsapp",
-	recipient_type: "individual",
-	type: "document",
-	to: "787025496788",
-	document: {
-		link: "https://www.africau.edu/images/default/sample.pdf",
-		filename: "hello.pdf",
-		caption: "Caption text...",
-	},
-};
+const file_link_path = "https://www.africau.edu/images/default/sample.pdf"; // todo
 
 const sendNotification = async (payload) => {
-	let response = await axios
+	await axios
 		.post(url, payload, {
 			headers: customHeaders,
 		})
-		.then(({ data }) => {
-			console.log(data);
+		.then(({ data, status }) => {
+			console.log("SUCCESS: ", status, data);
+			return data;
 		})
 		.catch((error) => {
-			console.error(error);
+			console.error(
+				"ERROR: ",
+				error.response.status,
+				error.response.statusText
+			);
 		});
-
-	console.log("response", response);
 };
 
-sendNotification(requestData);
+var requestData = {
+	messaging_product: "whatsapp",
+	recipient_type: "individual",
+	type: "document",
+};
 
 fs.readdir(directory, (err, files) => {
 	files.forEach((file) => {
-		// get the details of the file
 		let fileDetails = fs.lstatSync(path.resolve(directory, file));
 		// check if the file is directory
 		if (fileDetails.isDirectory()) {
 			// console.log("Directory: " + file);
 		} else {
-			console.log("File: " + file);
+			// console.log("File: " + file);
 			const parts = file.split("_");
 			const telephoneNumber = parts[2];
-
-			console.log("telephoneNumber", telephoneNumber);
+			requestData.to = `7${telephoneNumber}`;
+			requestData.document = {
+				link: file_link_path,
+				filename: file,
+				caption: "Caption text...",
+			};
+			sendNotification(requestData);
 		}
 	});
 });
